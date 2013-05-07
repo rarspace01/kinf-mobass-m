df426173bc320132713aa99972f8c107bf40c114
rarspace01 <rarspace01@gmail.com>
2013-05-07 07:24:20 +0000

    *ADD: -ex16 --intent filters --regex for geo uri *FIX: -wrong map location startup

--- gen/edu/denishamann/guesstimate/R.java
+++ gen/edu/denishamann/guesstimate/R.java
@@ -24,21 +24,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080001;
-        public static final int action_settings=0x7f08000d;
-        public static final int latitudeView=0x7f080004;
-        public static final int longitudeView=0x7f080006;
-        public static final int mapview=0x7f080002;
-        public static final int providerView=0x7f08000a;
-        public static final int qualityView=0x7f08000c;
-        public static final int satView=0x7f080008;
-        public static final int show_gps=0x7f08000e;
+        public static final int RelativeLayout1=0x7f080003;
+        public static final int action_settings=0x7f08000f;
+        public static final int geoIntentButton=0x7f080002;
+        public static final int latitudeView=0x7f080006;
+        public static final int longitudeView=0x7f080008;
+        public static final int mapview=0x7f080004;
+        public static final int providerView=0x7f08000c;
+        public static final int qualityView=0x7f08000e;
+        public static final int satView=0x7f08000a;
+        public static final int show_gps=0x7f080010;
         public static final int textView1=0x7f080000;
-        public static final int textView10=0x7f08000b;
-        public static final int textView2=0x7f080003;
-        public static final int textView4=0x7f080005;
-        public static final int textView6=0x7f080007;
-        public static final int textView8=0x7f080009;
+        public static final int textView10=0x7f08000d;
+        public static final int textView2=0x7f080005;
+        public static final int textView4=0x7f080007;
+        public static final int textView6=0x7f080009;
+        public static final int textView8=0x7f08000b;
+        public static final int webIntentButton=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
