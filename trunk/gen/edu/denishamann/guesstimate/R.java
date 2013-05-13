957f048edfa333fdc9014743f60e72cabe62119d
denishamann@gmail.com <denishamann@gmail.com@f1234a9f-3ff8-b7a2-2e6b-c937990b26fd>
2013-05-13 14:23:05 +0000

    *FIX: -android manifest merged *ADD: -background service on main activity

--- trunk/gen/edu/denishamann/guesstimate/R.java
+++ trunk/gen/edu/denishamann/guesstimate/R.java
@@ -21,25 +21,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int curloc=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080003;
-        public static final int action_settings=0x7f08000f;
+        public static final int RelativeLayout1=0x7f080005;
+        public static final int TextView01=0x7f080000;
+        public static final int action_settings=0x7f080011;
+        public static final int bind_service=0x7f080015;
         public static final int geoIntentButton=0x7f080002;
-        public static final int latitudeView=0x7f080006;
-        public static final int longitudeView=0x7f080008;
-        public static final int mapview=0x7f080004;
-        public static final int providerView=0x7f08000c;
-        public static final int qualityView=0x7f08000e;
-        public static final int satView=0x7f08000a;
-        public static final int show_gps=0x7f080010;
-        public static final int textView1=0x7f080000;
-        public static final int textView10=0x7f08000d;
-        public static final int textView2=0x7f080005;
-        public static final int textView4=0x7f080007;
-        public static final int textView6=0x7f080009;
-        public static final int textView8=0x7f08000b;
+        public static final int latitudeView=0x7f080008;
+        public static final int longitudeView=0x7f08000a;
+        public static final int mapview=0x7f080006;
+        public static final int providerView=0x7f08000e;
+        public static final int qualityView=0x7f080010;
+        public static final int read_value=0x7f080017;
+        public static final int satView=0x7f08000c;
+        public static final int show_gps=0x7f080012;
+        public static final int start_service=0x7f080013;
+        public static final int stop_service=0x7f080014;
+        public static final int textView10=0x7f08000f;
+        public static final int textView2=0x7f080007;
+        public static final int textView4=0x7f080009;
+        public static final int textView6=0x7f08000b;
+        public static final int textView8=0x7f08000d;
+        public static final int tvValue=0x7f080004;
+        public static final int unbind_service=0x7f080016;
+        public static final int valueDescr=0x7f080003;
         public static final int webIntentButton=0x7f080001;
     }
     public static final class layout {
@@ -55,6 +63,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bind_service=0x7f050010;
         public static final int empty=0x7f050009;
         public static final int gpsStatus=0x7f050007;
         public static final int hello_world=0x7f050002;
@@ -63,10 +72,14 @@ public final class R {
         public static final int maps_status_txt=0x7f050004;
         public static final int prov=0x7f05000d;
         public static final int qual=0x7f05000c;
+        public static final int read_value=0x7f050011;
         public static final int sat=0x7f05000b;
         public static final int show_gps=0x7f050005;
+        public static final int start_service=0x7f05000e;
+        public static final int stop_service=0x7f05000f;
         public static final int title_activity_map=0x7f050003;
         public static final int title_activity_show_gps=0x7f050006;
+        public static final int unbind_service=0x7f050012;
     }
     public static final class style {
         /** 
