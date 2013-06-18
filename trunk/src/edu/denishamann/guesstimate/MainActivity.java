6bb7e31da2e848a7e8d7172f24a3352f9956a41d
paul.birkenkampf@gmail.com <paul.birkenkampf@gmail.com@f1234a9f-3ff8-b7a2-2e6b-c937990b26fd>
2013-06-18 08:30:32 +0000

    updated activities

--- trunk/src/edu/denishamann/guesstimate/MainActivity.java
+++ trunk/src/edu/denishamann/guesstimate/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.denishamann.guesstimate;
 
-import edu.denishamann.guesstimate.BackgroundService.BackgroundServiceBinder;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
