fb287fc667eb49ae86ed2e58fd56ba947743f9bf
paul.birkenkampf@gmail.com <paul.birkenkampf@gmail.com@f1234a9f-3ff8-b7a2-2e6b-c937990b26fd>
2013-07-03 09:18:49 +0000

    after adding the guesses to the map markers, the map markers disappear, a proximity point appears and the map is zoomed so that you can see your current location and the proximity point

---
+++ src/edu/denishamann/guesstimate/OsmItemizedOverlay.java
@@ -0,0 +1,40 @@
+package edu.denishamann.guesstimate;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+public class OsmItemizedOverlay extends ItemizedIconOverlay<OverlayItem> {
+	private ArrayList<OverlayItem>	mItemList	= new ArrayList<OverlayItem>();
+
+	public OsmItemizedOverlay(ArrayList<OverlayItem> pList,
+			ItemizedIconOverlay.OnItemGestureListener<OverlayItem> pOnItemGestureListener, ResourceProxy pResourceProxy) {
+		super(pList, pOnItemGestureListener, pResourceProxy);
+		mItemList = pList;
+	}
+
+	public void addOverlay(OverlayItem aOverlayItem) {
+		mItemList.add(aOverlayItem);
+		populate();
+	}
+
+	public void removeOverlay(OverlayItem aOverlayItem) {
+		mItemList.remove(aOverlayItem);
+		populate();
+	}
+
+	public void removeOverlayByTitle(String title) {
+		Iterator<OverlayItem> iterator = mItemList.iterator();
+		while (iterator.hasNext()) {
+			OverlayItem curItem = iterator.next();
+			if (curItem.mTitle.equals(title)) {
+				iterator.remove();
+				break;
+			}
+		}
+		populate();
+	}
+}
