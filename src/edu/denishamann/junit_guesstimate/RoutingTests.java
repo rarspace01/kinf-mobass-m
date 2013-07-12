e5f94667996aadce5e8a1807bbc92833eb6cb9bd
rarspace01 <rarspace01@gmail.com>
2013-07-12 07:33:49 +0000

    *FIX: -testcases *DOC: -testcases

--- src/edu/denishamann/junit_guesstimate/RoutingTests.java
+++ src/edu/denishamann/junit_guesstimate/RoutingTests.java
@@ -1,13 +1,21 @@
 package edu.denishamann.junit_guesstimate;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import android.test.ActivityUnitTestCase;
 import android.util.Log;
 import edu.denishamann.guesstimate.activitys.MainActivity;
 import edu.denishamann.guesstimate.model.GeoLocation;
 import edu.denishamann.guesstimate.route.Route;
-import junit.framework.TestCase;
 
-public class RoutingTests extends ActivityUnitTestCase<MainActivity>  {
+/**
+ * testcase class for basic routing tests
+ * 
+ * @author denis
+ * 
+ */
+public class RoutingTests extends ActivityUnitTestCase<MainActivity> {
 
 	public RoutingTests() {
 		super(MainActivity.class);
@@ -18,13 +26,23 @@ public class RoutingTests extends ActivityUnitTestCase<MainActivity>  {
 		super.setUp();
 	}
 
-	public void testRoute(){
-		Route pr=new Route(new GeoLocation(49.904005,10.859725), new GeoLocation(49.902637,10.870646));
-		pr.getPath();
-		Log.i("JUNIT", "testcase1");
-		assertEquals("bla","bla");
+	/**
+	 * test method for the route test
+	 */
+	public void testRoute() {
+		Log.i("JUNIT", "Routing test started");
+		// create route with known points
+		Route pr = new Route(new GeoLocation(49.904005, 10.859725),
+				new GeoLocation(49.902637, 10.870646));
+		List<GeoLocation> listLocations = new LinkedList<GeoLocation>();
+		// retrieve path
+		listLocations = pr.getPath();
+
+		// check maneuver count == 7
+		// assertEquals(listLocations.size(),7);
+		assertTrue(listLocations.size() > 0);
 	}
-	
+
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
